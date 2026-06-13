729d3e5440877bfe223441fd27e71cf334710224
Rakibul Hasan <rakib932676@gmail.com>
2026-06-13 21:22:27 +0600

    Table Create and Data insert

---
+++ QUERT.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE assignment
+
+
+-- 1. Create User Table
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(30) NOT NULL CHECK (
+        role IN ('Ticket Manager', 'Football Fan')
+    ),
+    phone_number VARCHAR(20)
+);
+
+-- Insert Data
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+select * from Users
+
+--2. Create Match Table
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(20) NOT NULL CHECK (
+        match_status IN (
+            'Available',
+            'Selling Fast',
+            'Sold Out',
+            'Postponed'
+        )
+    )
+)
+
+--Insert values
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+select * from Matches
+
+--3. Create Booking Table
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20) CHECK (
+        payment_status IN (
+            'Pending',
+            'Confirmed',
+            'Cancelled',
+            'Refunded'
+        )
+    ),
+    total_cost DECIMAL(10,2) NOT NULL CHECK (total_cost >= 0),
+
+    FOREIGN KEY (user_id)
+        REFERENCES Users(user_id),
+
+    FOREIGN KEY (match_id)
+        REFERENCES Matches(match_id)
+);
+
+--Insert values
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
+
+select * from Bookings
